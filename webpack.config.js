c8c5e880a158b0e38f5f0f23f4d81d2bf0fe2627
Yahor Ramanenka <egoruch96@gmail.com>
2018-04-22 12:07:39 +0300

    babel + webpack configured

---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+
+const exampleName = 'hello-world';
+const exampleBaseFolder = path.join(__dirname, `examples/${exampleName}/`);
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    bundle: ['babel-polyfill', `./examples/${exampleName}/app.jsx`],
+  },
+  output: {
+    path: path.join(exampleBaseFolder, 'dist'),
+    filename: '[name].[hash].js',
+    publicPath: '/',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(exampleBaseFolder, 'src'),
+    hot: true,
+    inline: true,
+    port: 3000,
+    historyApiFallback: true,
+    compress: true,
+    open: true,
+    https: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(scss|sass|css)$/,
+        use: ['style-loader', 'css-loader', {
+          loader: 'sass-loader',
+          options: {
+            includePaths: [
+              './node_modules',
+            ],
+          },
+        }],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `examples/${exampleName}/index.html`,
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'defer',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+  ],
+};
