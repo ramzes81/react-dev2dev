e14c660d4de00ef99a9247a47928c69c7353df9c
Yahor Ramanenka <egoruch96@gmail.com>
2018-04-22 12:12:23 +0300

    added hello world example

---
+++ examples/hello-world/app.jsx
@@ -0,0 +1,11 @@
+import React from './../../src/react';
+import ReactDOM from './../../src/react-dom';
+
+const app = (
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is test.</p>
+  </div>
+);
+
+ReactDOM.render(app, document.getElementById('app'));
