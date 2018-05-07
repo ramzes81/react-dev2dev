e2da5dbd24453d17fba0a2197e8fadca3b240346
Yahor Ramanenka <egoruch96@gmail.com>
2018-05-08 01:04:05 +0300

    added applying of dom properties and listeners

--- src/react-dom/index.js
+++ src/react-dom/index.js
@@ -1,4 +1,33 @@
 import Symbols from './../react/symbols';
+import CONSTANTS from './../react/constants';
+
+function addDomEventListenerToElement(name, value, element) {
+  element.addEventListener(name.toLowerCase().substring(2), value);
+  return element;
+}
+
+function addAttributeToElement(name, value, element) {
+  if (typeof element.setAttribute === 'function') {
+    element.setAttribute(name, value);
+  } else {
+    // eslint-disable-next-line
+    element[name] = value;
+  }
+  return element;
+}
+
+const isListener = propName => propName.startsWith('on');
+const isAttribute = propName => !isListener(propName) && propName !== CONSTANTS.PROP_NAMES.CHILDREN;
+
+function applyPropsToDom(dom, props) {
+  const propKeys = Object.keys(props);
+  propKeys
+    .filter(isListener)
+    .forEach(propName => addDomEventListenerToElement(propName, props[propName], dom));
+  propKeys
+    .filter(isAttribute)
+    .forEach(propName => addAttributeToElement(propName, props[propName], dom));
+}
 
 function render(element, container) {
   const {
@@ -9,6 +38,8 @@ function render(element, container) {
   const dom = isTextElement
     ? document.createTextNode(element.props.nodeValue)
     : document.createElement(type);
+
+  applyPropsToDom(dom, element.props);
 
   const children = element.props.children || [];
   children.forEach(child => render(child, dom));
---
+++ src/react/constants.js
@@ -0,0 +1,7 @@
+const PROP_NAMES = {
+  CHILDREN: 'children',
+};
+
+export default {
+  PROP_NAMES,
+};
